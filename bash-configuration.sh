540613c7790420f1096f9f0e6756eca1879a6a7d
florianmarkusse <florianmarkusse@hotmail.com>
2023-08-10 21:32:44 +0200

    basic config

--- bash-configuration.sh
+++ bash-configuration.sh
@@ -4,4 +4,4 @@ echo "# custom terminal profiles" >> ~/.basrc
 echo "alias ssh="gnome-terminal --window-with-profile=SSH -- 'ssh'"" >> ~.basrc
 
 
-
+echo "export WEZTERM_CONFIG_FILE=$HOME/.config/nvim/wezterm.lua" >> ~/.basrc
--- install.sh
+++ install.sh
@@ -70,8 +70,6 @@ go install mvdan.cc/sh/v3/cmd/shfmt@latest
 echo -e "${YELLOW}Installing shellcheck... ${NO_COLOR}"
 sudo apt install shellcheck
 
-
-
 echo -e "${YELLOW}Setting up bash configuration... ${NO_COLOR}"
 ./bash-configuration.sh
 
