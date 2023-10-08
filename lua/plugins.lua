52090463c4e06926815028bcd4c1b948dfa98e5e
florianmarkusse <florianmarkusse@hotmail.com>
2023-10-08 21:06:07 +0200

    update clangtidy config

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -30,4 +30,14 @@ return {
 		build = ":call mkdp#util#install()",
 		event = "VeryLazy",
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end,
+	},
 }
