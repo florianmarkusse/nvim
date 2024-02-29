ede63e4f4c8eefbcffbb0eb8aebf206679d0d633
florianmarkusse <florianmarkusse@hotmail.com>
2024-02-29 21:01:41 +0100

    easier reokae

--- init.lua
+++ init.lua
@@ -125,3 +125,25 @@ vim.api.nvim_set_keymap(
 	":b#<CR>",
 	{ noremap = true, silent = true }
 )
+
+-- get contents of visual selection
+-- handle unpack deprecation
+table.unpack = table.unpack or unpack
+function get_visual()
+	local _, ls, cs = table.unpack(vim.fn.getpos("v"))
+	local _, le, ce = table.unpack(vim.fn.getpos("."))
+	return vim.api.nvim_buf_get_text(0, ls - 1, cs - 1, le - 1, ce, {})
+end
+
+vim.keymap.set("v", "<leader>r", function()
+	local pattern = table.concat(get_visual())
+	-- escape regex and line endings
+	pattern = vim.fn.substitute(
+		vim.fn.escape(pattern, "^$.*\\/~[]"),
+		"\n",
+		"\\n",
+		"g"
+	)
+	-- send parsed substitution command to command line
+	vim.api.nvim_input("<Esc>:%s/" .. pattern .. "//<Left>")
+end)
