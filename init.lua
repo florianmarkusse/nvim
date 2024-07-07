9b6d8ea2a32536a0f1b737a6cef4b245065690c1
florianmarkusse <florianmarkusse@hotmail.com>
2024-07-07 21:08:43 +0200

    more greppy

--- init.lua
+++ init.lua
@@ -414,13 +414,27 @@ require("lazy").setup({
 			"nvim-telescope/telescope.nvim",
 			tag = "0.1.2",
 			event = "VeryLazy",
-			dependencies = { "nvim-lua/plenary.nvim" },
+			dependencies = {
+				"nvim-lua/plenary.nvim",
+				"nvim-telescope/telescope-live-grep-args.nvim",
+			},
 			config = function()
+				local telescope = require("telescope")
 				local builtin = require("telescope.builtin")
 				vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 				vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+				vim.keymap.set("n", "<leader>fr", builtin.grep_string, {})
 				vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
 				vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
+				-- then load the extension
+				telescope.load_extension("live_grep_args")
+				vim.keymap.set(
+					"n",
+					"<leader>fd",
+					":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+					{}
+				)
 			end,
 		},
 		{
