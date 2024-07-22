97690f34ebf19dbf6f8b25cc8e41984034b5fd0e
florianmarkusse <florianmarkusse@hotmail.com>
2024-07-22 22:27:38 +0200

    da oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,36 @@
+return {
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		cmd = "Oil",
+		keys = { { "<leader>no", "<cmd>Oil --float<cr>", desc = "Oil buffer" } },
+		enabled = true,
+
+		-- {{{ Option
+		opts = function(_, opts)
+			opts.columns = { "icon" }
+			opts.prompt_save_on_select_new_entry = true
+			opts.use_default_keymaps = false
+
+			opts.keymaps = {
+				["g?"] = "actions.show_help",
+				["q"] = "actions.close",
+				["r"] = "actions.refresh",
+				["-"] = "actions.parent",
+				["_"] = "actions.open_cwd",
+				["`"] = "actions.cd",
+				["~"] = "actions.tcd",
+				["g."] = "actions.toggle_hidden",
+			}
+		end,
+
+		-- ----------------------------------------------------------------------- }}}
+		-- {{{ Configuration
+
+		config = function(_, opts)
+			require("oil").setup(opts)
+		end,
+
+		-- ----------------------------------------------------------------------- }}}
+	},
+}
